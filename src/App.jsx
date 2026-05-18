41d74ccd4c7b7fce34a677612183e9f5e28ea48e
Elodie CHANCEREL <elodie.chancerel.44@gmail.com>
2026-05-18 11:36:09 +0200

    Exo P3

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
+import { useState, useEffect } from 'react'
+
 import Banner from './components/Banner'
 import Cart from './components/Cart'
 import Footer from './components/Footer'
 import ShoppingList from './components/ShoppingList'
-
 import logo from './assets/logo.png'
-
+import './styles/Layout.css'
+import './styles/index.css'
 
 const App = () => {
+    const savedCart = localStorage.getItem('cart')
+    const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
     return (
         <div>
             <Banner>
-                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
                 <h1 className='lmj-title'>La maison jungle</h1>
             </Banner>
-            {/* <Cart /> */}
-            <ShoppingList />
+            <div className='lmj-layout-inner'>
+                <Cart cart={cart} updateCart={updateCart} />
+                <ShoppingList cart={cart} updateCart={updateCart} />
+            </div>
             <Footer />
         </div>
     )
